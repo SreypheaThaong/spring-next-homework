fa73d1c9acaa687ca0b703e3dcf8aecc937453e9
solen <solen0918@gmail.com>
2025-09-15 02:22:15 +0700

    feature: register and login v1

--- src/main/java/com/example/spring_jwt_example/controller/testing.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_jwt_example.controller;
-
-public class testing {
-    public String testing(){
-        return "testing";
-    }
-}
--- src/main/java/com/example/spring_jwt_example/controller/testing1.java
+++
@@ -1,7 +0,0 @@
-package com.example.spring_jwt_example.controller;
-
-public class testing1 {
-    public String testing(){
-        return "testing1";
-    }
-}
